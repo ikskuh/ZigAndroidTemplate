7fdad4c040c4636d371a39de33f1f963b504eacf
Louis Pearson <opensource@louispearson.work>
2022-10-21 00:58:53 -0600

    Switch from jarsigner to apksigner

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   outputs = { self, nixpkgs, devshell, flake-utils, android, zig }:
     {
       overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk android-studio zig;
+        inherit (self.packages.${final.system}) android-sdk zig;
       };
     }
     //
@@ -37,17 +37,13 @@
             emulator
             platform-tools
             platforms-android-30
+            ndk-25-1-8937393
 
             # Other useful packages for a development environment.
             # sources-android-30
             # system-images-android-30-google-apis-x86
             # system-images-android-30-google-apis-playstore-x86
           ]);
-
-          android-studio = pkgs.androidStudioPackages.stable;
-          # android-studio = pkgs.androidStudioPackages.beta;
-          # android-studio = pkgs.androidStudioPackages.preview;
-          # android-studio = pkgs.androidStudioPackage.canary;
         };
 
         devShell = import ./devshell.nix { inherit pkgs; };
